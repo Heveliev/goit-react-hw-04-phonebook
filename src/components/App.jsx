4258cefa525854e594819c6a0c4a48f04996798b
Heveliev-destress <tolik05.03@ukr.net>
2023-02-21 22:33:39 +0200

    fix useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
 const App = () => {
-  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) ??
+  const [contacts, setContacts] = useState(()=> JSON.parse(localStorage.getItem('contacts')) ??
     [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
